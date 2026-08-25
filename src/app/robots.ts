12407383940d3d46d785d3b9b51846e23d10cd17
Sudhir Kumar <kumar.sudhir@orangemantra.in>
2026-08-25 20:48:15 +0530

    feat: add dedicated sitemap index at /sitemap-index.xml to resolve built-in nextjs sitemap conflicts

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -7,6 +7,6 @@ export default function robots(): MetadataRoute.Robots {
       userAgent: "*",
       allow: "/"
     },
-    sitemap: `${siteConfig.url}/sitemap.xml`
+    sitemap: `${siteConfig.url.replace(/\/$/, "")}/sitemap-index.xml`
   };
 }
